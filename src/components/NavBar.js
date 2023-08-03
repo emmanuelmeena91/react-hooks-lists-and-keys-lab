5ea29b49304cadade7e1cfcae6a2afc47c52985f
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-08-03 11:08:02 +0300

    complete assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
-import React from "react";
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
+import React from 'react';
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+const NavBar = () => {
+  const links = ['home', 'about', 'projects'];
+
+  return (
+    <nav>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,15 @@
 import React from "react";
-
-function ProjectItem({ name, about, technologies }) {
+const ProjectItem = ({ technologies }) => {
   return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+    <div>
+      <h1>MY projects</h1>
+      <h3>Project Title</h3>
+      <p>Technologies:</p>
+      {technologies.map((technology, index) => (
+        <span key={index}>{technology}</span>
+      ))}
     </div>
   );
-}
+};
 
 export default ProjectItem;
